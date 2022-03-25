fd4ed719a25df082f902308c5868c16cb1e12c69
darwin-dh <darwinquishpe93@gmail.com>
2022-03-25 18:37:28 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const app = new Vue({
+    el:'#main',
+    data() {
+        return {
+         mostrar:false,
+         mostrar1:false,
+         mostrar2:false,
+         mostrar3:false,
+         mostrar4:false,
+         mostrar5:false,
+         mostrar6:false,
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+   
+    },
+})
